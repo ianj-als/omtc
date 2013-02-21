524ab97bcfd0bf06d3ba41523ad197b188f2d3c1
Ian Johnson <ian.johnson@appliedlanguage.com>
2013-02-21 17:08:54 +0000

    Ticket equals() and hashCode() implementations added.

--- src/main/java/com/capitati/omtc/core/scheduling/Ticket.java
+++ src/main/java/com/capitati/omtc/core/scheduling/Ticket.java
@@ -63,4 +63,31 @@ public abstract class Ticket<V> {
   public V getPriority() {
     return priority;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((identifier == null) ? 0 : identifier.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if(this == obj)
+      return true;
+    if(obj == null)
+      return false;
+    if(getClass() != obj.getClass())
+      return false;
+    @SuppressWarnings("unchecked")
+    Ticket<V> other = (Ticket<V> )obj;
+    if(identifier == null) {
+      if(other.identifier != null)
+        return false;
+    } else if(!identifier.equals(other.identifier))
+      return false;
+    return true;
+  }
 }
